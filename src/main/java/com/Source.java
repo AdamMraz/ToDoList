4937667c746e7ed502339f520e6a3701a9b82d8d
AdamMraz <59054958+AdamMraz@users.noreply.github.com>
2021-04-21 09:38:29 +0700

    Swagger added.

--- src/main/java/com/Source.java
+++ src/main/java/com/Source.java
@@ -1,5 +1,6 @@
 package com;
 
+import com.config.SwaggerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
--- src/main/java/com/rest/CaseManagerRESTApi.java
+++ src/main/java/com/rest/CaseManagerRESTApi.java
@@ -4,6 +4,10 @@ import com.dto.CaseDTO;
 import com.model.Case;
 import com.service.CaseManagerService;
 import com.service.ConverterDtoToModel;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +18,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "/api/case/")
+@Api(value = "Case Manager", description = "Api для работы с делами")
 public class CaseManagerRESTApi {
     private final CaseManagerService caseManagerService;
 
@@ -24,12 +29,14 @@ public class CaseManagerRESTApi {
 
     //Получение списка всех дел
     @GetMapping
+    @ApiOperation(value = "Получение списка дел")
     public ResponseEntity getCasesList() {
         return ResponseEntity.ok(caseManagerService.getCasesList());
     }
 
     //Создание нового дела
     @PostMapping
+    @ApiOperation(value = "Создание нового дела")
     public ResponseEntity addCase(CaseDTO newCase) {
         int id = caseManagerService.addCase(ConverterDtoToModel.ConverterDtoToModel(newCase));
         return new ResponseEntity(id, HttpStatus.OK);
@@ -37,6 +44,7 @@ public class CaseManagerRESTApi {
 
     //Удаление всех дел
     @DeleteMapping
+    @ApiOperation(value = "Удаление всех дел")
     public ResponseEntity deleteAllCases() {
         caseManagerService.deleteAllCases();
         return new ResponseEntity(HttpStatus.OK);
@@ -44,6 +52,7 @@ public class CaseManagerRESTApi {
 
     //Массовое обновление списка дел
     @PutMapping
+    @ApiOperation(value = "Массовое обновление списка дел")
     public ResponseEntity updateCasesList(CaseDTO caseDTOList) {
         System.out.println(caseDTOList.getId() + " " + caseDTOList.getValue() + " " + caseDTOList.getDeadLine());
         List<Case> caseList = new ArrayList<>();
@@ -63,6 +72,7 @@ public class CaseManagerRESTApi {
 
     //Возврат дела по id
     @GetMapping(value = "{id}")
+    @ApiOperation(value = "Возврад дела по id")
     public ResponseEntity getCase(@PathVariable int id) {
         Case newCase = caseManagerService.getCase(id);
 
@@ -74,6 +84,7 @@ public class CaseManagerRESTApi {
 
     //Удаление дела по id
     @DeleteMapping(value = "{id}")
+    @ApiOperation(value = "Удаление дела по id")
     public ResponseEntity deleteCase(@PathVariable int id) {
         if(caseManagerService.deleteCase(id)) {
             return new ResponseEntity(HttpStatus.OK);
@@ -89,6 +100,7 @@ public class CaseManagerRESTApi {
 
     //Обновление дела по id
     @PutMapping(value = "{id}")
+    @ApiOperation(value = "Обновление дела по id")
     public ResponseEntity putCase(@PathVariable int id, Case newCase) {
         newCase.setId(id);
         if(caseManagerService.updateCase(newCase)) {
@@ -97,7 +109,3 @@ public class CaseManagerRESTApi {
         return new ResponseEntity(HttpStatus.NOT_FOUND);
     }
 }
-
-
-
-
